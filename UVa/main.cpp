8ccf77368c1a2e189d00c7f0230349824c627899
Alexis Quinto <alexisgquinto@gmail.com>
2020-09-13 18:09:31 +0800

    Initial Commit

---
+++ UVa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UVa
+//
+//  Created by Alexis Quinto on 9/13/20.
+//  Copyright © 2020 Alexis Quinto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
