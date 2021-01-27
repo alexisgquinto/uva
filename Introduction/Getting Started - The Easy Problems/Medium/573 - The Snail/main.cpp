46322d06d45974230599d19f7eae112c763aaf26
Alexis Quinto <alexisgquinto@gmail.com>
2021-01-27 10:14:54 +0800

    Solution for 573

---
+++ Introduction/Getting Started - The Easy Problems/Medium/573 - The Snail/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  573 - The Snail
+//
+//  Created by Alexis Quinto on 1/26/21.
+//  Copyright © 2021 Alexis Quinto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
