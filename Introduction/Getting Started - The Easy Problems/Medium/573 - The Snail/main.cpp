5b5dada6913ab683ba911d73fc5f3255685867a5
Alexis Quinto <alexisgquinto@gmail.com>
2021-01-27 10:15:12 +0800

    Solution for 573

--- Introduction/Getting Started - The Easy Problems/Medium/573 - The Snail/main.cpp
+++ Introduction/Getting Started - The Easy Problems/Medium/573 - The Snail/main.cpp
@@ -8,8 +8,54 @@
 
 #include <iostream>
 
+using namespace std;
+
+bool hasReachedGoal(double y, double height) {
+    return (y < 0 || y > height);
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // freopen("input.txt", "r", stdin);
+    
+    double h, u, d, f;
+    cin >> h;
+    
+    while (h != 0) {
+        cin >> u >> d >> f;
+        
+        int day = 0;
+        double distanceClimbed = 0;
+        
+        double ff = u * (f/100);
+        
+        while (distanceClimbed >= 0 && distanceClimbed <= h) {
+            if (u - ff >= 0) {
+                if (day >= 1) {
+                    distanceClimbed += u - ff;
+                    u -= ff;
+                } else {
+                    distanceClimbed += u;
+                }
+                
+                if (hasReachedGoal(distanceClimbed, h)) break;
+                
+                distanceClimbed -= d;
+                
+                if (hasReachedGoal(distanceClimbed, h)) break;
+            } else {
+                distanceClimbed -= d;
+                if (hasReachedGoal(distanceClimbed, h)) break;
+            }
+            day++;
+        }
+        
+        if (distanceClimbed >= h) {
+            cout << "success on day " << day + 1 << '\n';
+        } else {
+            cout << "failure on day " << day + 1 << '\n';
+        }
+        
+        cin >> h;
+    }
+    
 }
