f7718f7774590eaec4e247f3832b819d5b51062c
Alexis Quinto <alexisgquinto@gmail.com>
2021-02-21 16:08:30 +0800

    Solution for 12478

---
+++ Introduction/Getting Started - The Easy Problems/Medium/12478 - Hardest Problem Ever (Easy)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  12478 - Hardest Problem Ever (Easy)
+//
+//  Created by Alexis Quinto on 2/21/21.
+//  Copyright © 2021 Alexis Quinto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "KABIR" << std::endl;
+    return 0;
+}
